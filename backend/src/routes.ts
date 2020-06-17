c3d3686b7a02b9665643bfb6382aa73cf50bf5d0
Filipe Melo <filipemelo032@gmail.com>
2020-06-17 10:00:03 -0300

    auth middleware added

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -14,21 +14,21 @@ const routes = Router();
 
 routes.post('/admins', Admin.create);
 routes.get('/admins/:id', auth, Admin.show);
-routes.put('/admins/:id', Admin.update);
-routes.delete('/admins/:id', Admin.destroy);
-routes.get('/admins', Admin.index);
+routes.put('/admins/:id', auth, Admin.update);
+routes.delete('/admins/:id', auth, Admin.destroy);
+routes.get('/admins', auth, Admin.index);
 routes.post('/admins/login', Session.create);
 
-routes.post('/products', Product.create);
+routes.post('/products', auth, Product.create);
 routes.get('/products', Product.index);
 routes.get('/products/:id', Product.show);
-routes.put('/products/:id', Product.update);
-routes.delete('/products/:id', Product.destroy);
+routes.put('/products/:id', auth, Product.update);
+routes.delete('/products/:id', auth, Product.destroy);
 
-routes.post('/categories', Category.create);
+routes.post('/categories', auth, Category.create);
 routes.get('/categories', Category.index);
 routes.get('/categories/:id', Category.show);
-routes.put('/categories/:id', Category.update);
-routes.delete('/categories/:id', Category.destroy);
+routes.put('/categories/:id', auth, Category.update);
+routes.delete('/categories/:id', auth, Category.destroy);
 
 export default routes;
